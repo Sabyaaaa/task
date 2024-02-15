bd53d60aa3daa10bed082be1339ea99fe1552ac3
Sabya <sabya7011@gmail.com>
2024-02-16 00:40:20 +0530

    scrolling down and error msg working fine

--- src/app/checkbox/checkbox.component.ts
+++ src/app/checkbox/checkbox.component.ts
@@ -1,15 +1,59 @@
-import { Component, OnInit } from '@angular/core';
+// checkbox.component.ts
+
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-checkbox',
   templateUrl: './checkbox.component.html',
-  styleUrls: ['./checkbox.component.css']
+  styleUrls: ['./checkbox.component.css'],
 })
-export class CheckboxComponent implements OnInit {
+export class CheckboxComponent {
+  showSubscribeMessage = false;
+  hasScrolledDown = false;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  @HostListener('window:scroll', ['$event'])
+  onScroll() {
+    this.checkScroll();
   }
 
+  onCheckboxChange(event: any) {
+    const isChecked = event.target.checked;
+
+    if (!isChecked) {
+      this.checkScroll();
+    } else {
+      this.showSubscribeMessage = false;
+    }
+  }
+
+  private checkScroll() {
+    const checkboxElement = document.getElementById(
+      'check-pos'
+    ) as HTMLInputElement;
+    const checkboxPosition = checkboxElement
+      ? checkboxElement.getBoundingClientRect()
+      : null;
+
+    if (window.scrollY > (checkboxPosition?.top || 0)) {
+      this.hasScrolledDown = true;
+    }
+
+    if (
+      this.hasScrolledDown &&
+      checkboxPosition &&
+      checkboxPosition.top >= 0 &&
+      checkboxPosition.bottom <= window.innerHeight
+    ) {
+      this.showSubscribeMessage = !this.isCheckboxChecked();
+    } else {
+      this.showSubscribeMessage = false;
+    }
+  }
+
+  private isCheckboxChecked(): boolean {
+    const checkboxElement = document.getElementById(
+      'check-pos'
+    ) as HTMLInputElement;
+    return checkboxElement ? checkboxElement.checked : false;
+  }
 }
